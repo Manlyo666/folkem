c8ddbc61821ad00aafc0614f7d31f7d9d3c3da28
Manlyo666 <a.miliani87@gmail.com>
2026-07-21 12:36:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='folkem-v1.0.9';
+const CACHE='folkem-v1.1.0';
 const ASSETS=['./','./index.html','./manifest.json',
   './assets/bg.jpg','./assets/cardback.png','./assets/otri/otre.png'];
 // aggiungo le maschere
